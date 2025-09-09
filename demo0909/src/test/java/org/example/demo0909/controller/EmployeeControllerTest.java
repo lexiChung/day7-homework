d8f3abc49db805be485100ae0f3f8d96f6cf40d7
Lexi Zhong <zhongle5@oocl.com>
2025-09-09 19:13:53 +0800

    test :get employee list

--- demo0909/src/test/java/org/example/demo0909/controller/EmployeeControllerTest.java
+++ demo0909/src/test/java/org/example/demo0909/controller/EmployeeControllerTest.java
@@ -39,5 +39,39 @@ class EmployeeControllerTest {
         .andExpect(jsonPath("$.id").value(1));
   }
 
+  @Test
+  void should_return_list_when_get_list()throws Exception{
+    String requestBody = """
+          {
+                "name" : "jenny",
+                "age" : 18,
+                "salary" : 5000,
+                "gender" :"female"
+          }""";
+    mockMvc.perform(post("/employee")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestBody))
+      .andExpect(status().isCreated())
+      .andExpect(jsonPath("$.id").value(1));
+
+    List<Employee> expect = new ArrayList<>();
+    Employee employee = new Employee();
+    employee.setId(1);
+    employee.setName("jenny");
+    employee.setAge(18);
+    employee.setSalary(5000);
+    employee.setGender("female");
+    expect.add(employee);
+
+    mockMvc.perform(get("/list")
+        .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$[0].id").value(expect.get(0).getId()))
+      .andExpect(jsonPath("$[0].name").value(expect.get(0).getName()))
+      .andExpect(jsonPath("$[0].age").value(expect.get(0).getAge()))
+      .andExpect(jsonPath("$[0].salary").value(expect.get(0).getSalary()))
+      .andExpect(jsonPath("$[0].gender").value(expect.get(0).getGender()));
+
+  }
+
 
 }
