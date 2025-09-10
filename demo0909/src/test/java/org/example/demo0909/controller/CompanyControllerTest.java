b2955d814859cea8437ca7b0dc4a8260931d32c0
Lexi Zhong <zhongle5@oocl.com>
2025-09-10 09:35:51 +0800

    feat:分页查询company

--- demo0909/src/test/java/org/example/demo0909/controller/CompanyControllerTest.java
+++ demo0909/src/test/java/org/example/demo0909/controller/CompanyControllerTest.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CompanyControllerTest {
 
   @Autowired
